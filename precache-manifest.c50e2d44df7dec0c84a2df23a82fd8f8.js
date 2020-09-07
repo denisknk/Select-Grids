de51ba28a4d996a627afae0fa8ec554fd9a5fad6
Denis <denisknk@gmail.com>
2020-09-07 14:51:14 +0300

    Updates

---
+++ precache-manifest.c50e2d44df7dec0c84a2df23a82fd8f8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2ab663e204fc4685e3a5c4ad65b2949",
+    "url": "/denisknk/Select-Grids/index.html"
+  },
+  {
+    "revision": "f51fe5d1658c4bfe2560",
+    "url": "/denisknk/Select-Grids/static/css/main.a377a7ad.chunk.css"
+  },
+  {
+    "revision": "63ebf2a8c9a35738c715",
+    "url": "/denisknk/Select-Grids/static/js/2.51f2f76f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/denisknk/Select-Grids/static/js/2.51f2f76f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f51fe5d1658c4bfe2560",
+    "url": "/denisknk/Select-Grids/static/js/main.c1e257ea.chunk.js"
+  },
+  {
+    "revision": "9bdac76affc924b9a0c3",
+    "url": "/denisknk/Select-Grids/static/js/runtime-main.178a0c08.js"
+  }
+]);
